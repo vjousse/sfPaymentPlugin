63b9f86b3137da3b81d5da9e5d94d848ee5de28d
marijn <marijn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-02 21:20:19 +0000

    Updated inline documentation.

--- lib/sfWebBrowserInterface.php
+++ lib/sfWebBrowserInterface.php
@@ -1,17 +1,13 @@
 <?php
 
   /**
-   * sfWebBrowserInterface is used to define the interface for fetching content.
-   * 
+   * sfWebBrowserInterface is used to define the interface for make requests to
+   * the gateway implementations.
+   *
    * @package   sfPaymentPlugin
    * @author    Marijn Huizendveld <marijn@round84.com>
    *
    * @version   $Revision$ changed by $Author$
-   */
-
-  /**
-   * The sfWebBrowserInterface interface defines the methods for fetching web
-   * content with a server-side web browser.
    */
   interface sfWebBrowserInterface
   {
@@ -19,45 +15,55 @@
     /**
      * Perform a HTTP GET request.
      *
-     * @param  string  $uri        The URI to send the request to.
-     * @param  array   $parameters The request parameters.
-     * @param  array   $headers    The headers to send with the request.
+     * @param   string                $uri        The URI to send the request to.
+     * @param   array                 $parameters The request parameters.
+     * @param   array                 $headers    The headers to send with the request.
+     *
+     * @return  sfWebBrowserInterface
      */
     function get ($uri, $parameters = array(), $headers = array());
 
     /**
      * Perform a HTTP HEAD request.
      *
-     * @param  string  $uri        The URI to send the request to.
-     * @param  array   $parameters The request parameters.
-     * @param  array   $headers    The headers to send with the request.
+     * @param   string                $uri        The URI to send the request to.
+     * @param   array                 $parameters The request parameters.
+     * @param   array                 $headers    The headers to send with the request.
+     *
+     * @return  sfWebBrowserInterface
      */
     function head ($uri, $parameters = array(), $headers = array());
 
     /**
      * Perform a HTTP POST request.
      *
-     * @param  string  $uri        The URI to send the request to.
-     * @param  array   $parameters The request parameters.
-     * @param  array   $headers    The headers to send with the request.
+     * @param   string                $uri        The URI to send the request to.
+     * @param   array                 $parameters The request parameters.
+     * @param   array                 $headers    The headers to send with the request.
+     *
+     * @return  sfWebBrowserInterface
      */
     function post ($uri, $parameters = array(), $headers = array());
 
     /**
      * Perform a HTTP PUT request.
      *
-     * @param  string  $uri        The URI to send the request to.
-     * @param  array   $parameters The request parameters.
-     * @param  array   $headers    The headers to send with the request.
+     * @param   string                $uri        The URI to send the request to.
+     * @param   array                 $parameters The request parameters.
+     * @param   array                 $headers    The headers to send with the request.
+     *
+     * @return  sfWebBrowserInterface
      */
     function put ($uri, $parameters = array(), $headers = array());
 
     /**
      * Perform a HTTP DELETE request.
      *
-     * @param  string  $uri        The URI to send the request to.
-     * @param  array   $parameters The request parameters.
-     * @param  array   $headers    The headers to send with the request.
+     * @param   string                $uri        The URI to send the request to.
+     * @param   array                 $parameters The request parameters.
+     * @param   array                 $headers    The headers to send with the request.
+     *
+     * @return  sfWebBrowserInterface
      */
     function delete ($uri, $parameters = array(), $headers = array());
 
