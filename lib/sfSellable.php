218bcbd89f88b278a2e17a342b5a2aecb53d0bd2
marijn <marijn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-10 10:45:25 +0000

    Added `sfSellable` interface for the `sfPaymentPlugin`.

---
+++ lib/sfSellable.php
@@ -0,0 +1,13 @@
+<?php
+
+  /**
+   * sfSellable.
+   *
+   * @package   sfPaymentPlugin
+   * @author    Marijn Huizendveld <marijn@round84.com>
+   *
+   * @version   $Revision: 24815 $ changed by $Author: marijn $
+   */
+  interface sfSellable
+  {
+  }
