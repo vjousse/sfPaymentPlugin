0bba7f1713d85024957a2f2c90914dd6735ec3a0
marijn <marijn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-12-17 11:59:10 +0000

    Added base form for the `sfPaymentPlugin`.

---
+++ lib/form/sfPaymentFormAbstract.php
@@ -0,0 +1,13 @@
+<?php
+
+  /**
+   * sfPaymentFormAbstract.
+   * 
+   * @package   sfPaymentPlugin
+   * @author    Marijn Huizendveld <marijn@round84.com>
+   *
+   * @version   $Revision: 20173 $ changed by $Author: marijn $
+   */
+  abstract class sfPaymentFormAbstract extends sfForm
+  {
+  }
