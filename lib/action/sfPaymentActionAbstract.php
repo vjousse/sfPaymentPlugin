9714aaaa56ccbde9f86bfe5c8f432d8819e945a7
marijn <marijn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-11 23:27:42 +0000

    [sfPaymentPlugin] Added `sfPaymentActionAbstract#processForm()` method for handling boiler plate form processing.

--- lib/action/sfPaymentActionAbstract.php
+++ lib/action/sfPaymentActionAbstract.php
@@ -10,4 +10,46 @@
    */
   abstract class sfPaymentActionAbstract extends sfAction
   {
+
+    /**
+     * Process a form object.
+     *
+     * @param   sfPaymentFormAbstract $arg_form     The form to process.
+     * @param   sfWebRequest          $arg_request  The request object.
+     * @param   String                $arg_route    The route to which the form data should be submitted.
+     * @param   String                $arg_method   The form submit method.
+     *                                
+     * @return  String                              The view type to render.
+     */
+    protected function _processForm (sfPaymentFormAbstract $arg_form, sfWebRequest $arg_request, $arg_route, $arg_method = 'post')
+    {
+      $name = $arg_form->getName();
+
+      if ($arg_request->isMethod($arg_method) && $arg_request->hasParameter($name))
+      {
+        $arg_form->bind($arg_request->getParameter($name), $arg_form->isMultipart() ? $arg_request->getFiles($name) : NULL);
+
+        if ($arg_form->isValid())
+        {
+          $this->redirect(array('sf_route'   => $arg_route
+                               ,'sf_subject' => $this->_doProcessForm($arg_form)
+                               ));
+        }
+      }
+
+      return sfView::INPUT;
+    }
+
+    /**
+     * Process a form object when it is valid.
+     *
+     * @param   sfPaymentFormAbstract $arg_form     The form to process.
+     *
+     * @return  mixed
+     */
+    protected function _doProcessForm (sfPaymentFormAbstract $arg_form)
+    {
+      return $arg_form->process();
+    }
+
   }
