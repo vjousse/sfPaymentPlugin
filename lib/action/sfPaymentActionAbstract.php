234df12741579b277693bc1f1cafbcddddd56e1a
marijn <marijn@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-11 23:23:50 +0000

    [sfPaymentPlugin] Added `sfPaymentActionAbstract` for shared logic amongst the controllers of the `sfPaymentPlugin`.

---
+++ lib/action/sfPaymentActionAbstract.php
@@ -0,0 +1,13 @@
+<?php
+
+  /**
+   * sfPaymentActionAbstract.
+   * 
+   * @author    Marijn Huizendveld <marijn@round84.com>
+   * @version   $Revision: 209 $ changed by $Author: marijn $
+   *
+   * @copyright Round 84 (2009)
+   */
+  abstract class sfPaymentActionAbstract extends sfAction
+  {
+  }
